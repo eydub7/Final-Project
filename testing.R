9a70f7078e00d5effa2fde448482880aebea1633
eydub7 <emmett_dubnoff@brown.edu>
2025-12-01 13:59:48 -0500

    Final Bug Fixes

---
+++ testing.R
@@ -0,0 +1,8 @@
+#testing/running code
+
+#load libraries 
+
+#read in data
+ridership_df <- read.csv("/Users/juliadubnoff/Downloads/ridership_simulated.csv")
+otp_df <- read.csv("/Users/juliadubnoff/Downloads/otp_simulated.csv")
+
